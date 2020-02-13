2f0bc262fe131c90410bb9b04b7530f9a5224c0b
Tarang Hirani <tarang9211@gmail.com>
2020-02-13 15:55:28 +1100

    fixes and refactor

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -8,6 +8,7 @@ import { Workspace, Check } from "./checks/utils";
 import { checks } from "./checks";
 import { ExitError } from "./errors";
 import { writeWorkspace } from "./utils";
+import { runCmd } from "./run";
 import spawn from "spawndamnit";
 import pLimit from "p-limit";
 
@@ -81,26 +82,39 @@ async function execCmd(args: string[]) {
   throw new ExitError(highestExitCode);
 }
 
-async function runCmd(args: string[]) {
-  let workspacesRoot = await findWorkspacesRoot(process.cwd());
-  let workspaces = (await getWorkspaces({
-    cwd: workspacesRoot,
-    tools: ["yarn", "bolt", "pnpm", "root"]
-  }))!;
-  // console.log(workspaces);
-  const matchingWorkspaces = workspaces.filter(workspace => {
-    return workspace.name.includes(args[0]);
-  });
+// async function runCmd(args: string[]) {
+//   let workspacesRoot = await findWorkspacesRoot(process.cwd());
+//   let workspaces = (await getWorkspaces({
+//     cwd: workspacesRoot,
+//     tools: ["yarn", "bolt", "pnpm", "root"]
+//   }))!;
 
-  if (matchingWorkspaces.length > 1) {
-  } else if (matchingWorkspaces.length === 0) {
-    console.warn("No matching packages found");
-    throw new ExitError(1);
-  } else {
-    const [workspace] = matchingWorkspaces;
-    await spawn("yarn", args, { cwd: workspacesRoot, stdio: "inherit" });
-  }
-}
+//   const matchingWorkspaces = workspaces.filter(workspace => {
+//     return (
+//       workspace.name.includes(args[0]) ||
+//       path.relative(workspacesRoot, workspace.dir).includes(args[0])
+//     );
+//   });
+
+//   if (matchingWorkspaces.length > 1) {
+//     logger.error(
+//       `an identifier must only match a single package but "${
+//         args[0]
+//       } matches the following packages: \n${matchingWorkspaces
+//         .map(x => x.name)
+//         .join("\n")}`
+//     );
+//     throw new ExitError(1);
+//   } else if (matchingWorkspaces.length === 0) {
+//     logger.error("No matching packages found");
+//     throw new ExitError(1);
+//   } else {
+//     await spawn("yarn", args.slice(1), {
+//       cwd: matchingWorkspaces[0].dir,
+//       stdio: "inherit"
+//     });
+//   }
+// }
 
 (async () => {
   let things = process.argv.slice(2);
