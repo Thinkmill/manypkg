61b7a99a9c2980bdeba67c4c8c95c9adfb7e6e2a
Tarang Hirani <tarang9211@gmail.com>
2020-03-06 13:51:21 +1100

    wip: tests

--- packages/get-packages/src/index.test.ts
+++ packages/get-packages/src/index.test.ts
@@ -2,8 +2,14 @@ import { getFixturePath } from "jest-fixtures";
 import { getPackages } from "./";
 
 describe("get-packages", () => {
-  it("should work", async () => {
+  it("should resolve workspaces for yarn", async () => {
     let url = await getFixturePath(__dirname, "yarn-workspace-base");
-    await getPackages(url);
+    const allPackages = await getPackages(url);
+    expect(allPackages.packages[0].packageJson.name).toEqual(
+      "yarn-workspace-base-pkg-a"
+    );
+    expect(allPackages.packages[1].packageJson.name).toEqual(
+      "yarn-workspace-base-pkg-b"
+    );
   });
 });
