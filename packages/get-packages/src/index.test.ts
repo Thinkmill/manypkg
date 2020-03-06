6d0f58a35e36b7ea734af8dbc8081255d2630892
Tarang Hirani <tarang9211@gmail.com>
2020-03-06 13:58:38 +1100

    wip: bolt tests

--- packages/get-packages/src/index.test.ts
+++ packages/get-packages/src/index.test.ts
@@ -3,13 +3,24 @@ import { getPackages } from "./";
 
 describe("get-packages", () => {
   it("should resolve workspaces for yarn", async () => {
-    let url = await getFixturePath(__dirname, "yarn-workspace-base");
-    const allPackages = await getPackages(url);
+    const path = await getFixturePath(__dirname, "yarn-workspace-base");
+    const allPackages = await getPackages(path);
     expect(allPackages.packages[0].packageJson.name).toEqual(
       "yarn-workspace-base-pkg-a"
     );
     expect(allPackages.packages[1].packageJson.name).toEqual(
       "yarn-workspace-base-pkg-b"
+    );
+  });
+
+  it("should resolve workspaces for bolt", async () => {
+    const path = await getFixturePath(__dirname, "bolt-workspace");
+    const allPackages = await getPackages(path);
+    expect(allPackages.packages[0].packageJson.name).toEqual(
+      "bolt-workspace-pkg-a"
+    );
+    expect(allPackages.packages[1].packageJson.name).toEqual(
+      "bolt-workspace-pkg-b"
     );
   });
 });
