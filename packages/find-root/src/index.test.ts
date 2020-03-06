724d7304d482dc293810d6b91a1e4c680db248ee
Tarang Hirani <tarang9211@gmail.com>
2020-03-06 18:41:57 +1100

    fixed tests

--- packages/find-root/src/index.test.ts
+++ packages/find-root/src/index.test.ts
@@ -1,27 +1,39 @@
-import { findRoot } from ".";
+import { findRoot, findRootSync } from ".";
 import fixturez from "fixturez";
 import path from "path";
 
 let f = fixturez(__dirname);
 
-test("it returns the root of a monorepo", async () => {
-  let tmpPath = f.copy("basic");
-  let workspacesRoot = await findRoot(
-    path.join(tmpPath, "packages", "package-one", "src")
-  );
-  expect(workspacesRoot).toBe(tmpPath);
+type FindRoot = typeof findRoot | typeof findRootSync;
+
+const runTests = (findRoot: FindRoot) => {
+  test("it returns the root of a monorepo", async () => {
+    let tmpPath = f.copy("basic");
+    let workspacesRoot = await findRoot(
+      path.join(tmpPath, "packages", "package-one", "src")
+    );
+    expect(workspacesRoot).toBe(tmpPath);
+  });
+
+  test("it returns the root of a pnpm monorepo", async () => {
+    let tmpPath = f.copy("basic-pnpm");
+    let workspacesRoot = await findRoot(
+      path.join(tmpPath, "packages", "package-one", "src")
+    );
+    expect(workspacesRoot).toBe(tmpPath);
+  });
+
+  test("it returns the root of a single-package repo", async () => {
+    let tmpPath = f.copy("single-pkg");
+    let workspacesRoot = await findRoot(path.join(tmpPath, "src"));
+    expect(workspacesRoot).toBe(tmpPath);
+  });
+};
+
+describe("findRoot", () => {
+  runTests(findRoot);
 });
 
-test("it returns the root of a pnpm monorepo", async () => {
-  let tmpPath = f.copy("basic-pnpm");
-  let workspacesRoot = await findRoot(
-    path.join(tmpPath, "packages", "package-one", "src")
-  );
-  expect(workspacesRoot).toBe(tmpPath);
-});
-
-test("it returns the root of a single-package repo", async () => {
-  let tmpPath = f.copy("single-pkg");
-  let workspacesRoot = await findRoot(path.join(tmpPath, "src"));
-  expect(workspacesRoot).toBe(tmpPath);
+describe("findRootSync", () => {
+  runTests(findRootSync);
 });
