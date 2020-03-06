74b9b3d5f05b6e6e50a8e03dc738a77a92c40e0f
Tarang Hirani <tarang9211@gmail.com>
2020-03-06 18:02:40 +1100

    more functional

--- packages/get-packages/src/index.test.ts
+++ packages/get-packages/src/index.test.ts
@@ -3,7 +3,9 @@ import { getPackages, getPackagesSync } from "./";
 
 const f = fixturez(__dirname);
 
-describe("getPackages", () => {
+type GetPackages = typeof getPackages | typeof getPackagesSync;
+
+let runTests = (getPackages: GetPackages) => {
   it("should resolve workspaces for yarn", async () => {
     const allPackages = await getPackages(f.copy("yarn-workspace-base"));
 
@@ -89,92 +91,12 @@ describe("getPackages", () => {
       );
     }
   });
+};
+
+describe("getPackages", () => {
+  runTests(getPackages);
 });
 
 describe("getPackagesSync", () => {
-  it("should resolve workspaces for yarn", () => {
-    const allPackages = getPackagesSync(f.copy("yarn-workspace-base"));
-
-    if (allPackages.packages === null) {
-      return expect(allPackages.packages).not.toBeNull();
-    }
-
-    expect(allPackages.packages[0].packageJson.name).toEqual(
-      "yarn-workspace-base-pkg-a"
-    );
-    expect(allPackages.packages[1].packageJson.name).toEqual(
-      "yarn-workspace-base-pkg-b"
-    );
-    expect(allPackages.tool).toEqual("yarn");
-  });
-
-  it("should resolve yarn workspaces if the yarn option is passed and packages field is used", () => {
-    const allPackages = getPackagesSync(f.copy("yarn-workspace-base"));
-
-    if (allPackages.packages === null) {
-      return expect(allPackages.packages).not.toBeNull();
-    }
-    expect(allPackages.packages[0].packageJson.name).toEqual(
-      "yarn-workspace-base-pkg-a"
-    );
-    expect(allPackages.packages[1].packageJson.name).toEqual(
-      "yarn-workspace-base-pkg-b"
-    );
-    expect(allPackages.tool).toEqual("yarn");
-  });
-
-  it("should resolve workspaces for bolt", () => {
-    const allPackages = getPackagesSync(f.copy("bolt-workspace"));
-
-    if (allPackages.packages === null) {
-      return expect(allPackages.packages).not.toBeNull();
-    }
-
-    expect(allPackages.packages[0].packageJson.name).toEqual(
-      "bolt-workspace-pkg-a"
-    );
-    expect(allPackages.packages[1].packageJson.name).toEqual(
-      "bolt-workspace-pkg-b"
-    );
-    expect(allPackages.tool).toEqual("bolt");
-  });
-
-  it("should resolve workspaces for pnpm", () => {
-    const allPackages = getPackagesSync(f.copy("pnpm-workspace-base"));
-
-    if (allPackages.packages === null) {
-      return expect(allPackages.packages).not.toBeNull();
-    }
-
-    expect(allPackages.packages[0].packageJson.name).toEqual(
-      "pnpm-workspace-base-pkg-a"
-    );
-    expect(allPackages.packages[1].packageJson.name).toEqual(
-      "pnpm-workspace-base-pkg-b"
-    );
-    expect(allPackages.tool).toEqual("pnpm");
-  });
-
-  it("should resolve the main package", () => {
-    const path = f.copy("root-only");
-    const allPackages = getPackagesSync(path);
-
-    if (allPackages.packages === null) {
-      return expect(allPackages.packages).not.toBeNull();
-    }
-
-    expect(allPackages.packages[0].dir).toEqual(path);
-    expect(allPackages.packages.length).toEqual(1);
-    expect(allPackages.tool).toEqual("root");
-  });
-
-  it("should throw an error if a package.json is missing the name field", async () => {
-    try {
-      const allPackages = getPackagesSync(f.copy("no-name-field"));
-    } catch (err) {
-      expect(err.message).toBe(
-        'The following package.jsons are missing the "name" field:\npackages/pkg-a/package.json\npackages/pkg-b/package.json'
-      );
-    }
-  });
+  runTests(getPackagesSync);
 });
