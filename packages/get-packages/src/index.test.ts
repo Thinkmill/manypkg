318ec8306e2f52aa581918644651c377a3abf23e
Tarang Hirani <tarang9211@gmail.com>
2020-03-06 15:03:56 +1100

    wip: sync tests

--- packages/get-packages/src/index.test.ts
+++ packages/get-packages/src/index.test.ts
@@ -1,5 +1,5 @@
 import fixturez from "fixturez";
-import { getPackages } from "./";
+import { getPackages, getPackagesSync } from "./";
 
 const f = fixturez(__dirname);
 
@@ -42,5 +42,69 @@ describe("getPackages", () => {
     const allPackages = await getPackages(path);
     expect(allPackages.packages[0].dir).toEqual(path);
     expect(allPackages.packages.length).toEqual(1);
+  });
+
+  it("should throw an error if a package.json is missing the name field", async () => {
+    const path = f.find("no-name-field");
+    try {
+      const allPackages = await getPackagesSync(path);
+    } catch (err) {
+      expect(err.message).toBe(
+        'The following package.jsons are missing the "name" field:\npackages/pkg-a/package.json\npackages/pkg-b/package.json'
+      );
+    }
+  });
+});
+
+describe("getPackagesSync", () => {
+  it("should resolve workspaces for yarn", () => {
+    const path = f.find("yarn-workspace-base");
+    const allPackages = getPackagesSync(path);
+    expect(allPackages.packages[0].packageJson.name).toEqual(
+      "yarn-workspace-base-pkg-a"
+    );
+    expect(allPackages.packages[1].packageJson.name).toEqual(
+      "yarn-workspace-base-pkg-b"
+    );
+  });
+
+  it("should resolve workspaces for bolt", () => {
+    const path = f.find("bolt-workspace");
+    const allPackages = getPackagesSync(path);
+    expect(allPackages.packages[0].packageJson.name).toEqual(
+      "bolt-workspace-pkg-a"
+    );
+    expect(allPackages.packages[1].packageJson.name).toEqual(
+      "bolt-workspace-pkg-b"
+    );
+  });
+
+  it("should resolve workspaces for pnpm", () => {
+    const path = f.find("pnpm-workspace-base");
+    const allPackages = getPackagesSync(path);
+    expect(allPackages.packages[0].packageJson.name).toEqual(
+      "pnpm-workspace-base-pkg-a"
+    );
+    expect(allPackages.packages[1].packageJson.name).toEqual(
+      "pnpm-workspace-base-pkg-b"
+    );
+  });
+
+  it("should resolve the main package", () => {
+    const path = f.find("root-only");
+    const allPackages = getPackagesSync(path);
+    expect(allPackages.packages[0].dir).toEqual(path);
+    expect(allPackages.packages.length).toEqual(1);
+  });
+
+  it("should throw an error if a package.json is missing the name field", async () => {
+    const path = f.find("no-name-field");
+    try {
+      const allPackages = getPackagesSync(path);
+    } catch (err) {
+      expect(err.message).toBe(
+        'The following package.jsons are missing the "name" field:\npackages/pkg-a/package.json\npackages/pkg-b/package.json'
+      );
+    }
   });
 });
