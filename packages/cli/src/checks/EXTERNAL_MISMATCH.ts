7088bad4797f84f5ecb3d1ed1c6b216036fade5c
Tarang Hirani <tarang9211@gmail.com>
2020-03-09 14:37:23 +1100

    rename: EXTERNAL_MISMATCH

--- packages/cli/src/checks/EXTERNAL_MISMATCH.ts
+++ packages/cli/src/checks/EXTERNAL_MISMATCH.ts
@@ -4,27 +4,28 @@ import {
   getHighestExternalRanges,
   NORMAL_DEPENDENCY_TYPES
 } from "./utils";
+import { Package } from "@manypkg/get-packages";
 
 type ErrorType = {
   type: "EXTERNAL_MISMATCH";
-  workspace: Workspace;
+  pkg: Package;
   dependencyName: string;
   dependencyRange: string;
   highestDependencyRange: string;
 };
 
 export default makeCheck<ErrorType>({
-  validate: (workspace, allWorkspaces) => {
+  validate: (pkg, allPackages) => {
     let errors: {
       type: "EXTERNAL_MISMATCH";
-      workspace: Workspace;
+      pkg: Package;
       dependencyName: string;
       dependencyRange: string;
       highestDependencyRange: string;
     }[] = [];
-    let highestExternalRanges = getHighestExternalRanges(allWorkspaces);
+    let highestExternalRanges = getHighestExternalRanges(allPackages);
     for (let depType of NORMAL_DEPENDENCY_TYPES) {
-      let deps = workspace.config[depType];
+      let deps = pkg.packageJson[depType];
       if (deps) {
         for (let depName in deps) {
           let range = deps[depName];
@@ -32,7 +33,7 @@ export default makeCheck<ErrorType>({
           if (highestRange !== undefined && highestRange !== range) {
             errors.push({
               type: "EXTERNAL_MISMATCH",
-              workspace,
+              pkg,
               dependencyName: depName,
               dependencyRange: range,
               highestDependencyRange: highestRange
@@ -45,7 +46,7 @@ export default makeCheck<ErrorType>({
   },
   fix: error => {
     for (let depType of NORMAL_DEPENDENCY_TYPES) {
-      let deps = error.workspace.config[depType];
+      let deps = error.pkg.packageJson[depType];
       if (deps && deps[error.dependencyName]) {
         deps[error.dependencyName] = error.highestDependencyRange;
       }
@@ -53,6 +54,6 @@ export default makeCheck<ErrorType>({
     return { requiresInstall: true };
   },
   print: error =>
-    `${error.workspace.name} has a dependency on ${error.dependencyName}@${error.dependencyRange} but the highest range in the repo is ${error.highestDependencyRange}, the range should be set to ${error.highestDependencyRange}`,
+    `${error.pkg.packageJson.name} has a dependency on ${error.dependencyName}@${error.dependencyRange} but the highest range in the repo is ${error.highestDependencyRange}, the range should be set to ${error.highestDependencyRange}`,
   type: "all"
 });
