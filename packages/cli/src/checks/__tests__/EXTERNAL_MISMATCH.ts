4ed37cde64aae839d29f8871af94842c4514e62a
ryan_wang5 <rwang5@atlassian.com>
2022-02-02 16:32:16 +1100

    minor fix - reduce diffs

--- packages/cli/src/checks/__tests__/EXTERNAL_MISMATCH.ts
+++ packages/cli/src/checks/__tests__/EXTERNAL_MISMATCH.ts
@@ -1,4 +1,4 @@
-import externalMismatch from "../EXTERNAL_MISMATCH";
+import internalMismatch from "../EXTERNAL_MISMATCH";
 import { getWS, getFakeWS, getRootWS } from "../../test-helpers";
 
 let rootWorkspace = getRootWS();
@@ -14,10 +14,10 @@ it("should error if the ranges are valid and they are not equal", () => {
   };
   ws.set("pkg-2", pkg2);
 
-  let errors = externalMismatch.validate(pkg2, ws, rootWorkspace, {});
+  let errors = internalMismatch.validate(pkg2, ws, rootWorkspace, {});
   expect(errors.length).toEqual(0);
 
-  errors = externalMismatch.validate(ws.get("pkg-1")!, ws, rootWorkspace, {});
+  errors = internalMismatch.validate(ws.get("pkg-1")!, ws, rootWorkspace, {});
   expect(errors.length).toEqual(1);
   expect(errors).toMatchInlineSnapshot(`
     [
@@ -59,7 +59,7 @@ it("should error and return the correct mostCommonDependencyRange when the range
   };
 
   ws.set("pkg-3", pkg3);
-  let errors = externalMismatch.validate(
+  let errors = internalMismatch.validate(
     ws.get("pkg-1")!,
     ws,
     rootWorkspace,
@@ -67,7 +67,7 @@ it("should error and return the correct mostCommonDependencyRange when the range
   );
   expect(errors.length).toEqual(0);
 
-  errors = externalMismatch.validate(pkg2, ws, rootWorkspace, {});
+  errors = internalMismatch.validate(pkg2, ws, rootWorkspace, {});
   expect(errors.length).toEqual(1);
   expect(errors).toMatchInlineSnapshot(`
     [
@@ -109,7 +109,7 @@ it("should error and return the correct mostCommonDependencyRange when the range
   };
 
   ws.set("pkg-3", pkg3);
-  let errors = externalMismatch.validate(
+  let errors = internalMismatch.validate(
     ws.get("pkg-1")!,
     ws,
     rootWorkspace,
@@ -139,7 +139,7 @@ it("should error and return the correct mostCommonDependencyRange when the range
     ]
   `);
 
-  errors = externalMismatch.validate(pkg2, ws, rootWorkspace, {});
+  errors = internalMismatch.validate(pkg2, ws, rootWorkspace, {});
   expect(errors.length).toEqual(0);
 });
 
@@ -160,7 +160,7 @@ it("should error and return the correct mostCommonDependencyRange when the range
   };
 
   ws.set("pkg-3", pkg3);
-  let errors = externalMismatch.validate(
+  let errors = internalMismatch.validate(
     ws.get("pkg-1")!,
     ws,
     rootWorkspace,
@@ -189,7 +189,7 @@ it("should error and return the correct mostCommonDependencyRange when the range
     ]
   `);
 
-  errors = externalMismatch.validate(pkg2, ws, rootWorkspace, {});
+  errors = internalMismatch.validate(pkg2, ws, rootWorkspace, {});
   expect(errors.length).toEqual(1);
   expect(errors).toMatchInlineSnapshot(`
     [
@@ -213,7 +213,7 @@ it("should error and return the correct mostCommonDependencyRange when the range
     ]
   `);
 
-  errors = externalMismatch.validate(pkg3, ws, rootWorkspace, {});
+  errors = internalMismatch.validate(pkg3, ws, rootWorkspace, {});
   expect(errors.length).toEqual(0);
 });
 
@@ -228,10 +228,10 @@ it("should not error if the value is not a valid semver range", () => {
   };
   ws.set("pkg-2", pkg2);
 
-  let errors = externalMismatch.validate(pkg2, ws, rootWorkspace, {});
+  let errors = internalMismatch.validate(pkg2, ws, rootWorkspace, {});
   expect(errors.length).toEqual(0);
 
-  errors = externalMismatch.validate(ws.get("pkg-1")!, ws, rootWorkspace, {});
+  errors = internalMismatch.validate(ws.get("pkg-1")!, ws, rootWorkspace, {});
   expect(errors.length).toEqual(0);
 });
 
@@ -252,10 +252,10 @@ it("should not error if the range is included in the allowedDependencyVersions o
     }
   };
 
-  let errors = externalMismatch.validate(pkg2, ws, rootWorkspace, options);
+  let errors = internalMismatch.validate(pkg2, ws, rootWorkspace, options);
   expect(errors.length).toEqual(0);
 
-  errors = externalMismatch.validate(
+  errors = internalMismatch.validate(
     ws.get("pkg-1")!,
     ws,
     rootWorkspace,
@@ -264,7 +264,7 @@ it("should not error if the range is included in the allowedDependencyVersions o
   expect(errors.length).toEqual(0);
 });
 
-it("should error if the range is the range is outside allowedDependencyVersions and running fix should clamp it to the most commonly used one", () => {
+it("should error if the range is outside allowedDependencyVersions and running fix should clamp it to the most commonly used one", () => {
   let ws = getWS();
 
   ws.get("pkg-1")!.packageJson.dependencies = { something: "1.0.0" };
@@ -295,7 +295,7 @@ it("should error if the range is the range is outside allowedDependencyVersions 
     }
   };
 
-  let errors = externalMismatch.validate(pkg3, ws, rootWorkspace, options);
+  let errors = internalMismatch.validate(pkg3, ws, rootWorkspace, options);
   expect(errors.length).toEqual(1);
   expect(errors[0]).toEqual(
     expect.objectContaining({
@@ -304,6 +304,6 @@ it("should error if the range is the range is outside allowedDependencyVersions 
     })
   );
 
-  externalMismatch.fix(errors[0], options);
+  internalMismatch.fix(errors[0], options);
   expect(pkg3.packageJson.dependencies.something).toEqual("1.0.0");
 });
