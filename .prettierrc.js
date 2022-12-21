0aa7c33be719cc0f3e5b5dfb1e2bc446b7e3751d
Mateusz Burzyński <mateuszburzynski@gmail.com>
2022-12-21 11:18:58 +0100

    Bump `@types/prettier`

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,6 @@
 // Documentation for this file: https://prettier.io/docs/en/configuration.html
+
+/** @type {import('prettier').Options} */
 module.exports = {
   // Use .gitattributes to manage newlines
   endOfLine: "auto",
@@ -6,10 +8,8 @@ module.exports = {
   // Avoid line breaks in markdown
   proseWrap: "never",
 
-  // Prettier plugins
-  //
-  // Disabling search dirs and pointing directly at folder paths relative to this config file
-  // makes it easier for VSCode and WebStorm to find your plugins.
-  pluginSearchDirs: [],
-  plugins: ["./node_modules/prettier-plugin-packagejson"]
+  // Disabling search dirs and pointing directly at plugins
+  // makes it easier for VSCode and WebStorm to find them.
+  pluginSearchDirs: false,
+  plugins: [require("prettier-plugin-packagejson")],
 };
