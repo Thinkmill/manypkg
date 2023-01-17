3a24a4689bb995419ed8ff04864abf07c6fbfa5b
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-01-17 12:34:04 +0100

    Exports only tools from `/tools` and handle default order in `/find-root`

--- packages/tools/src/index.ts
+++ packages/tools/src/index.ts
@@ -1,4 +1,3 @@
-import { Tool, ToolType } from "./Tool";
 import { BoltTool } from "./BoltTool";
 import { LernaTool } from "./LernaTool";
 import { PnpmTool } from "./PnpmTool";
@@ -6,42 +5,5 @@ import { RootTool } from "./RootTool";
 import { RushTool } from "./RushTool";
 import { YarnTool } from "./YarnTool";
 
-/**
- * A convenient mapping of tool type names to tool implementations.
- */
-const supportedTools: Record<ToolType, Tool> = {
-  bolt: BoltTool,
-  lerna: LernaTool,
-  pnpm: PnpmTool,
-  root: RootTool,
-  rush: RushTool,
-  yarn: YarnTool,
-};
-
-/**
- * A default ordering for monorepo tool checks.
- *
- * This ordering is designed to check the most typical package.json-based
- * monorepo implementations first, with tools based on custom file scchemas
- * checked last.
- */
-const defaultOrder: ToolType[] = [
-  "yarn",
-  "bolt",
-  "pnpm",
-  "lerna",
-  "root",
-  "rush",
-];
-
 export * from "./Tool";
-export {
-  BoltTool,
-  LernaTool,
-  PnpmTool,
-  RootTool,
-  RushTool,
-  YarnTool,
-  supportedTools,
-  defaultOrder,
-};
+export { BoltTool, LernaTool, PnpmTool, RootTool, RushTool, YarnTool };
