a3d710f8d396a71714fc30674ff448442c52c20d
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-01-23 14:47:17 +0100

    Declare `package.json#engines` and bump transpilaton target

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,15 @@
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-typescript"],
-  plugins: ["@babel/plugin-transform-runtime"]
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: { node: 14 },
+      },
+    ],
+    "@babel/preset-typescript",
+  ],
+  // keep this just in case - it's not needed for this project right now
+  // if we ever start depending on some helpers again, then we can reconsider adding `@babel/runtime` as a dependency
+  // or removing this plugin altogether
+  plugins: ["@babel/plugin-transform-runtime"],
 };
