585493847a530bd87540eb0207b337d8ea5d05c6
Jason Gallavin <4037224+jasekiw@users.noreply.github.com>
2025-08-28 12:50:33 -0400

    Keep detected line endings flavor of `package.json` files on Windows when updating those files (#260)
    * fix: line endings are now respected on windows
    
    * Update .changeset/clever-feet-move.md
    
    Co-authored-by: Mateusz Burzyński <mateuszburzynski@gmail.com>
    
    * Add tests for respecting line endings
    
    * Update .changeset/clever-feet-move.md
    
    ---------
    
    Co-authored-by: Mateusz Burzyński <mateuszburzynski@gmail.com>

---
+++ .changeset/clever-feet-move.md
@@ -0,0 +1,5 @@
+---
+"@manypkg/cli": patch
+---
+
+Keep detected line endings flavor of `package.json` files on Windows when updating those files
--- packages/cli/src/run.test.ts
+++ packages/cli/src/run.test.ts
@@ -2,6 +2,8 @@ import { describe, expect, it } from "vitest";
 import fixturez from "fixturez";
 import stripAnsi from "strip-ansi";
 import { exec } from "tinyexec";
+import fs from "node:fs";
+import path from "node:path";
 
 const f = fixturez(__dirname);
 
@@ -28,18 +30,11 @@ describe("Run command", () => {
   ])(
     'should execute "%s %s" and exit with %i',
     async (arg0, arg1, expectedExitCode) => {
-      const { exitCode, stdout, stderr } = await exec(
-        "node",
-        [require.resolve("../bin.js"), "run", arg0, arg1],
-        {
-          nodeOptions: {
-            cwd: f.find("basic-with-scripts"),
-            env: {
-              ...process.env,
-              NODE_OPTIONS: "--experimental-strip-types",
-            },
-          },
-        }
+      const { exitCode, stdout, stderr } = await executeBin(
+        f.find("basic-with-scripts"),
+        "run",
+        arg0,
+        arg1
       );
       expect(exitCode).toBe(expectedExitCode);
       expect(stripAnsi(stdout.toString())).toMatchSnapshot("stdout");
@@ -49,3 +44,64 @@ describe("Run command", () => {
     }
   );
 });
+
+describe("Fix command", () => {
+  it.each([
+    ["package-one", "fix", "lf", 0],
+    ["package-one", "fix", "crlf", 0],
+  ] as const satisfies [string, string, LineEndings, number][])(
+    'should execute "%s %s" without changing line endings from %s',
+    async (arg0, arg1, sourceLineEnding, expectedExitCode) => {
+      // arrange
+      const temp = f.copy("basic-with-scripts");
+      const filePath = path.join(temp, "package.json");
+      convertFileLineEndings(filePath, sourceLineEnding);
+      // act
+      const { exitCode } = await executeBin(temp, "fix", arg0, arg1);
+      // assert
+      expect(exitCode).toBe(expectedExitCode);
+      const fixedPackageFile = fs.readFileSync(filePath, "utf8");
+      f.cleanup();
+      expect(detectLineEndings(fixedPackageFile)).toBe(sourceLineEnding);
+    }
+  );
+});
+
+type LineEndings = "crlf" | "lf";
+
+function convertFileLineEndings(path: string, targetLineEnding: LineEndings) {
+  let file = fs.readFileSync(path, "utf8");
+  // detect mixed line endings
+  if (
+    file.includes("\r\n") &&
+    (file.match(/\r\n/g) || []).length !== (file.match(/\n/g) || []).length
+  ) {
+    throw new Error("mixed line endings in fixture file: " + path);
+  }
+  // if the line endings match, we don't need to convert the file
+  if (file.includes("\r\n") === (targetLineEnding === "crlf")) {
+    return;
+  }
+  const sourceLineEndingText = targetLineEnding === "crlf" ? "\n" : "\r\n";
+  const targetLineEndingText = targetLineEnding === "crlf" ? "\r\n" : "\r\n";
+  fs.writeFileSync(
+    path,
+    file.replaceAll(sourceLineEndingText, targetLineEndingText)
+  );
+}
+
+function executeBin(path: string, command: string, ...args: string[]) {
+  return exec("node", [require.resolve("../bin.js"), command, ...args], {
+    nodeOptions: {
+      cwd: path,
+      env: {
+        ...process.env,
+        NODE_OPTIONS: "--experimental-strip-types",
+      },
+    },
+  });
+}
+
+function detectLineEndings(content: string) {
+  return (content.includes("\r\n") ? "crlf" : "lf") satisfies LineEndings;
+}
